fb99077523de051b835d6492a7c1f7e106c416ba
allen-tran <allentran08@gmail.com>
2022-08-30 22:17:47 -0700

    refactored and added functionality for web application

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from re import L
-from flask import Flask, Response, request
+from flask import Flask, request, render_template, redirect
 import photo_scrapper
 import video_scrapper
 
@@ -8,22 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return Response(status=200, mimetype='application/json')
+    return render_template("index.html")
 
 
-@app.route("/fetchObject/", methods=["POST"])
-def scrape():
-    user_url = request.json
-    print(user_url['link'])
-
-    response_link = video_scrapper.scrape_video(user_url['link'])
+@app.route('/fetch', methods=["POST"])
+def home_form():
+    print(request.form)
+    user_url = request.form['text']
+    print(user_url)
+    response_link = video_scrapper.scrape_video(user_url)
     if response_link:
         return
 
-    response_link = photo_scrapper.scrape_photo(user_url['link'])
+    response_link = photo_scrapper.scrape_photo(user_url)
 
-    return Response(f"{response_link}", status=200, mimetype='application/json')
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
--- photo_scrapper.py
+++ photo_scrapper.py
@@ -12,15 +12,10 @@ def scrape_photo(user_url):
     if not images:
         return None
 
-    filename = generate_time()
     for img in images:
         request_link = f"http:{img['src']}"
         image_data = requests.get(request_link).content
-        with open(filename, 'wb') as handler:
+        with open('.\\static\\vsco_image.jpg', 'wb') as handler:
             handler.write(image_data)
 
     return jsonify({"link": request_link})
-
-
-def generate_time():
-    return f"{time.time()}.jpg"
